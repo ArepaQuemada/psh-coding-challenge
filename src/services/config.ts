ffa9091df1d197e029724b09787108c191ad1568
Juan <rigualjuan@gmail.com>
2022-03-21 15:58:28 -0300

    feat: services config and axios instance

---
+++ src/services/config.ts
@@ -0,0 +1,15 @@
+const config = {
+    baseUrl: 'https://www.officeapi.dev',
+    endpoints: {
+        characters: {
+            name: '/characters/',
+            method: 'get'   
+        },
+        crew: {
+            name: '/crew/',
+            method: 'get'
+        }
+    }
+}
+
+export default config
